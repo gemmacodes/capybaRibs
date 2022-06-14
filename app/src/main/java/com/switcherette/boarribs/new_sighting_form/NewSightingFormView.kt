e99d39aac8b28b7176d23729eb434962e6ffd3c1
switcherette <gfornons@gmail.com>
2022-06-14 13:14:19 +0200

    WIP

--- app/src/main/java/com/switcherette/boarribs/new_sighting_form/NewSightingFormView.kt
+++ app/src/main/java/com/switcherette/boarribs/new_sighting_form/NewSightingFormView.kt
@@ -1,11 +1,23 @@
 package com.switcherette.boarribs.new_sighting_form
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.core.content.FileProvider
 import com.badoo.ribs.core.customisation.inflate
 import com.badoo.ribs.core.view.AndroidRibView
 import com.badoo.ribs.core.view.RibView
 import com.badoo.ribs.core.view.ViewFactory
+import com.bumptech.glide.Glide
 import com.jakewharton.rxrelay2.PublishRelay
 import com.switcherette.boarribs.R
 import com.switcherette.boarribs.databinding.RibNewSightingFormBinding
@@ -13,6 +25,8 @@ import com.switcherette.boarribs.new_sighting_form.NewSightingFormView.Event
 import com.switcherette.boarribs.new_sighting_form.NewSightingFormView.ViewModel
 import io.reactivex.ObservableSource
 import io.reactivex.functions.Consumer
+import java.io.File
+import java.io.IOException
 
 interface NewSightingFormView : RibView,
     ObservableSource<Event>,
@@ -26,9 +40,10 @@ interface NewSightingFormView : RibView,
             val interaction: Boolean,
             val comments: String?,
             val picture: String?
-        ): Event()
+        ) : Event()
 
-        object TakePicture: Event()
+        object TakePhoto: Event()
+        object ChoosePhotoFromGallery: Event()
 
     }
 
@@ -62,18 +77,69 @@ class NewSightingFormViewImpl private constructor(
             )
     }
 
-    val binding: RibNewSightingFormBinding = RibNewSightingFormBinding.bind(androidView)
+    private val binding: RibNewSightingFormBinding = RibNewSightingFormBinding.bind(androidView)
 
     override fun accept(vm: ViewModel) {
-        with(binding){
+        with(binding) {
+
             val heading = etHeading.text.toString().trim()
             val adults = etNumAdults.text.toString()
             val piglets = etNumPiglets.text.toString()
             val interaction = btnSEnvironment.isChecked
             var comments = etComment.text.toString().trim()
             if (comments.isEmpty()) {
-                comments = "no description provided"
+                comments = context.getString(R.string.no_comments)
             }
+            val picturePath = null
+
+            fabAddForm.setOnClickListener {
+                if (heading.isNotEmpty() && adults.isNotEmpty() && piglets.isNotEmpty()) {
+                    events.accept(
+                        Event.SaveSighting(
+                            heading,
+                            adults.toInt(),
+                            piglets.toInt(),
+                            interaction,
+                            comments,
+                            picturePath
+                        )
+                    )
+                } else {
+                    Toast.makeText(
+                        context,
+                        "'Title', 'Adults' and 'Piglets' are mandatory fields!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            btnAddPicture.setOnClickListener {
+                showConfirmationDialog(it)
+            }
+
+
         }
     }
+
+    private fun showConfirmationDialog(view: View) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(view.context)
+        builder.setTitle(context.getString(R.string.add_photo))
+
+        val options = arrayOf<CharSequence>(
+            context.getString(R.string.take_photo),
+            context.getString(R.string.choose_gallery),
+            context.getString(R.string.cancel_photo)
+        )
+        builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+            if (options[item] == context.getString(R.string.take_photo)) {
+                events.accept(Event.TakePhoto)
+            } else if (options[item] == context.getString(R.string.choose_gallery)) {
+                events.accept(Event.ChoosePhotoFromGallery)
+            } else if (options[item] == context.getString(R.string.cancel_photo)) {
+                dialog.dismiss()
+            }
+        })
+        builder.show()
+    }
+
 }
--- app/src/main/java/com/switcherette/boarribs/new_sighting_form/feature/NewSightingFormFeature.kt
+++ app/src/main/java/com/switcherette/boarribs/new_sighting_form/feature/NewSightingFormFeature.kt
@@ -1,6 +1,11 @@
 package com.switcherette.boarribs.new_sighting_form.feature
 
+import android.content.Intent
 import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.badoo.mvicore.element.Actor
 import com.badoo.mvicore.element.Bootstrapper
 import com.badoo.mvicore.element.NewsPublisher
@@ -12,6 +17,9 @@ import com.switcherette.boarribs.data.SightingsDataSource
 import com.switcherette.boarribs.new_sighting_form.feature.NewSightingFormFeature.*
 import io.reactivex.Observable
 import io.reactivex.Observable.empty
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 internal class NewSightingFormFeature(
@@ -48,16 +56,19 @@ internal class NewSightingFormFeature(
             val comments: String?,
             val picture: String?
         ) : Wish()
+
+        object TakePhoto : Wish()
+        object ChoosePhotoFromGallery : Wish()
     }
 
     sealed class Effect {
         object SightingSaved : Effect()
-        object SightingIncomplete : Effect()
+        data class PhotoTaken(val picturePath: String) : Effect()
+        data class PhotoFromGalleryChosen(val picturePath: String) : Effect()
     }
 
     sealed class News {
-        object SightingSaved: News()
-        object SightingNotSaved: News()
+        object SightingSaved : News()
     }
 
     class BootStrapperImpl : Bootstrapper<Wish> {
@@ -73,54 +84,104 @@ internal class NewSightingFormFeature(
         override fun invoke(state: State, wish: Wish): Observable<Effect> =
             when (wish) {
                 is Wish.SaveSighting -> saveForm(dataSource, wish)
+                Wish.ChoosePhotoFromGallery -> selectFromCamera()
+                Wish.TakePhoto -> dispatchTakePictureIntent()
             }
 
         private fun saveForm(
             dataSource: SightingsDataSource,
             wish: Wish.SaveSighting
         ): Observable<Effect> {
-            return if (
-                wish.heading == null
-                || wish.adults == null
-                || wish.piglets == null
-                || wish.comments == null
-            ) {
-                Observable.just(Effect.SightingIncomplete)
-            } else {
-                dataSource.saveSighting(
-                    Sighting(
-                        id = UUID.randomUUID().toString(),
-                        heading = wish.heading,
-                        adults = wish.adults,
-                        piglets = wish.piglets,
-                        interaction = wish.interaction,
-                        comments = wish.comments,
-                        latitude = latitude,
-                        longitude = longitude,
-                        timestamp = System.currentTimeMillis(),
-                        picture = wish.picture
-                            ?: Uri.parse("android.resource://com.switcherette.boarribs/" + R.drawable.boar_img)
-                                .toString()
-                    )
+            dataSource.saveSighting(
+                Sighting(
+                    id = UUID.randomUUID().toString(),
+                    heading = wish.heading!!,
+                    adults = wish.adults!!,
+                    piglets = wish.piglets!!,
+                    interaction = wish.interaction,
+                    comments = wish.comments!!,
+                    latitude = latitude,
+                    longitude = longitude,
+                    timestamp = System.currentTimeMillis(),
+                    picture = wish.picture
+                        ?: Uri.parse("android.resource://com.switcherette.boarribs/" + R.drawable.boar_img)
+                            .toString()
                 )
-                Observable.just(Effect.SightingSaved)
+            )
+            return Observable.just(Effect.SightingSaved)
+        }
+
+
+        private fun selectFromCamera(): Observable<Effect> {
+            Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+            }.let {
+                startActivityForResult(Intent.createChooser(it, "Select Picture"), 2)
+                return Observable.just(Effect.PhotoFromGalleryChosen(it.data.toString()))
             }
         }
+
+        private fun dispatchTakePictureIntent(): Observable<Effect> {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                // Ensure that there's a camera activity to handle the intent
+                takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        Toast.makeText(requireContext(), "Error, ouch!", Toast.LENGTH_SHORT).show()
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            requireContext(),
+                            "com.switcherette.boarribs.fileprovider",
+                            it
+                        )
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        startActivityForResult(takePictureIntent, 1)
+                    }
+                    return if (photoFile != null) {
+                        return Observable.just(Effect.PhotoTaken(photoFile.absolutePath))
+                    } else Observable.empty()
+                }
+
+            }
+
+        }
+    }
+
+    @Throws(IOException::class)
+    fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        )
     }
 
     class ReducerImpl : Reducer<State, Effect> {
         override fun invoke(state: State, effect: Effect): State =
             when (effect) {
-                Effect.SightingSaved -> TODO()
-                Effect.SightingIncomplete -> TODO()
+                Effect.SightingSaved -> state
+                is Effect.PhotoFromGalleryChosen -> state.copy(picture = effect.picturePath)
+                is Effect.PhotoTaken -> state.copy(picture = effect.picturePath)
             }
     }
 
     class NewsPublisherImpl : NewsPublisher<Wish, Effect, State, News> {
         override fun invoke(wish: Wish, effect: Effect, state: State): News? =
             when (effect) {
-                Effect.SightingIncomplete -> News.SightingNotSaved
                 Effect.SightingSaved -> News.SightingSaved
+                is Effect.PhotoFromGalleryChosen -> TODO()
+                is Effect.PhotoTaken -> TODO()
             }
     }
 }
